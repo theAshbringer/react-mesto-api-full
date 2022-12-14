854ab6fee4b4a271e29dd52b73808bbac714b890
theAshbringer <kristya1310@gmail.com>
2022-12-14 20:55:07 +0700

    feat: crash test route

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,13 @@ app.use(cookieParser());
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(routes);
 
 app.use(errorLogger);
